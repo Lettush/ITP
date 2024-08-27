4ab745e367ac029da2cc0330f8213939aa369af8
Jared Hortaleza <jaredneilhortaleza11@gmail.com>
2024-08-27 16:18:13 +0800

    changed file structure

---
+++ Week 2/01-variables.js
@@ -0,0 +1,27 @@
+console.log("Hello World!");
+
+// var - global (anywhere)
+var firstName = "Myrtile";
+firstName = "Catherine";
+console.log("First Name:", firstName);
+
+// let - local (inside {})
+let lastName = "Cayanan";
+lastName = "Sarmiento";
+console.log("Last Name:", lastName);
+
+if (true) {
+    var email = "johndoe@gmail.com";
+    let userName = "johndoe";
+
+    console.log("Email:", email);
+    console.log("Username:", userName);
+}
+
+console.log("Email:", email);
+// console.log("Username:", userName); - ERROR
+
+// const
+const funds = 0;
+// funds = 200; - ERROR
+console.log("Funds:", funds);
